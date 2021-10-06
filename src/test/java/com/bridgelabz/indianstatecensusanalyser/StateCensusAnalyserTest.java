9a71861ed1b078c53a83930428d2f923d8b28593
777shetty <shetty.chetu7@gmail.com>
2021-10-06 21:31:18 +0530

    ADDED:Test case to validate exception thrown when file path is invalid

--- src/test/java/com/bridgelabz/indianstatecensusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/indianstatecensusanalyser/StateCensusAnalyserTest.java
@@ -7,14 +7,21 @@ import java.io.IOException;
 import static org.junit.Assert.assertEquals;
 
 public class StateCensusAnalyserTest {
-    public static final String INCORRECT_CSV_FILE = "./incorrectFilePath.csv";
-    public static final String CORRECT_CSV_FILE = "/home/rajatsp/bridgelabz/day29/assignment/IndianStatesCensusAnalyser/IndianStateCensusData.csv";
-    public static final String CSV_FILE_INDIA_STATE_CODE = "IndiaStateCode";
+	 public static final String FILE_NOT_EXIST = "incorrectFilePath.csv";
+    public static final String CORRECT_CSV_FILE = "//home/chethan/perfios/code/IndianStateCensusAnalyserr/IndianStateCensusData.csv";
+   
 
     @Test
-    public void givenIndianCensusCSVFile_ShouldReturnCorrectRecords() throws IOException {
-        StateCensusAnalyser analyser = new StateCensusAnalyser();
-        int count = analyser.loadIndianStateCensusData(CORRECT_CSV_FILE);
-        assertEquals(29, count);
-    }
+    	public void givenIndianCensusCSVFile_ShouldReturnCorrectRecords() throws IOException, StateCensusAnalyserException {
+            StateCensusAnalyser analyser = new StateCensusAnalyser();
+            int count = analyser.loadIndianStateCensusData(CORRECT_CSV_FILE);
+            assertEquals(29, count);
+        }
+
+        @Test
+        public void givenCSVFile_ReturnException_ifFileNotPresent() throws IOException {
+            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+			censusAnalyser.loadIndianStateCensusData(FILE_NOT_EXIST);
+        }
+
 }
